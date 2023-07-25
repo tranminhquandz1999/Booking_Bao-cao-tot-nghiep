35c0c2cb386e55b3618de9e54ab7e52e4442476b
tranminhquandz1999 <tranminhquandz1999@gmail.com>
2023-07-25 21:49:52 +0700

    #37

--- Backend-NodeJS-Quickstart-master/src/route/web.js
+++ Backend-NodeJS-Quickstart-master/src/route/web.js
@@ -17,6 +17,7 @@ let initWebRoutes = (app) => {
 
   // router.post("/api/login", userController.handleLogin);
   router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
 
   return app.use("/", router);
 };
